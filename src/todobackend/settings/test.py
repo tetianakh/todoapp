15dc8122916738c92666b67d346ef65d2e0d0caa
Tetiana Khotiaintseva <tetiana.khot@gmail.com>
2017-05-06 21:59:38 +0200

    Test settings

---
+++ src/todobackend/settings/test.py
@@ -0,0 +1,32 @@
+import os
+
+from base import *  # noqa
+
+INSTALLED_APPS += ('django_nose', )
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR', '.')
+NOSE_ARGS = [
+    '--verbosity=2',
+    '--nologcapture',
+    '--with-coverage',
+    '--cover-package=todo',
+    '--with-spec',
+    '--spec-color',
+    '--with-xunit',
+    '--xunit-file=%s/unittest.xml' % TEST_OUTPUT_DIR,
+    '--cover-xml',
+    '--cover-xml-file=%s/coverage.xml' % TEST_OUTPUT_DIR,
+]
+
+
+# Test database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE', 'todobackend'),
+        'USER': os.environ.get('MYSQL_USER', 'todo'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+    }
+}
--- src/todobackend/wsgi.py
+++ src/todobackend/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todobackend.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "todobackend.settings.base")
 
 application = get_wsgi_application()
