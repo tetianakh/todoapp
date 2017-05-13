ed0a16e091098d5c8dc8086a6dccd1ae6659650e
Tetiana Khotiaintseva <tetiana.khot@gmail.com>
2017-05-14 00:17:58 +0200

    Add setup.py

---
+++ src/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "todoapp",
+    version = "0.1.0",
+    description = "Todo backend Django REST service",
+    packages = find_packages(),
+    include_package_data = True,
+    scripts = ["manage.py"],
+    install_requires = [
+        "Django>=1.11.1,<1.12",
+        "django-cors-headers>=2.0.2",
+        "djangorestframework>=3.6.2",
+        "MySQL-python>=1.2.5",
+    ],
+    extras_require = {
+        "test": [
+            "colorama>=0.3.9",
+            "coverage>=4.3.4",
+            "django-nose>=1.4.4",
+            "nose>=1.3.7",
+            "pinocchio>=0.4.2",
+        ],
+    },
+)
